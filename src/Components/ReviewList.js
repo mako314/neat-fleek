d8ef37c7a5b4eaf509db22bed4fc2a4ae078f620
Bryant Wiggins <topwigit@gmail.com>
2023-06-03 13:46:30 -0400

    Added left side menu bar

--- src/Components/ReviewList.js
+++ src/Components/ReviewList.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReviewCards from './ReviewCards';
 
 function ReviewList(){
+    const [gameReviews, setGameReviews] = useState([])
+
+    useEffect(() =>{
+        fetch("http://localhost:3000/reviews")
+        .then(resp => resp.json())
+        .then(data => setGameReviews(data))
+      },[])
+
+      console.log(gameReviews)
+
+      const mappedGameReviews = gameReviews.map((gameReview) =>(
+        <ReviewCards/>
+      ))
+
+
     return (
         <ul className="cards">{<ReviewCards/>}</ul>
     )
