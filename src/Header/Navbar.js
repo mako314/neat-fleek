4dca52a3e11510ec2f2851ab1523064025f1b4a2
Bryant Wiggins <topwigit@gmail.com>
2023-06-03 14:47:11 -0400

    randomly added search icon

--- src/Header/Navbar.js
+++ src/Header/Navbar.js
@@ -4,7 +4,7 @@ import {FaSearch} from "react-icons/fa";
 function Navbar () {
     return (
     <div className="fixed top-0 left-0 h-screen w-auto m-0 flex flex-col bg-gray-900 text-white shadow-lg">
-     <Search searchIcon={<FaSearch size="28" />}/> 
+     <Search searchIcon={<FaSearch size="28" color="red" />}/> 
     </div>
     )
 }
