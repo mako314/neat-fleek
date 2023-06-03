ef9da7aa4c7f53b5afdb0b56dca3091a5812c697
Bryant Wiggins <topwigit@gmail.com>
2023-06-03 14:47:11 -0400

    randomly added search icon

--- src/Header/Search.js
+++ src/Header/Search.js
@@ -2,9 +2,9 @@
 
 function Search ({searchIcon}) {
     return (
-        <div>
-            <form>
-                <input className="flex-auto border-black bg-slate-600" type="search" placeholder="Search..." ></input>{searchIcon}
+        <div >
+            <form> {searchIcon}
+                <input className="flex-auto border-black bg-slate-600 top-10"  type="search" placeholder="Search..." ></input>
             </form>
         </div>
     )
