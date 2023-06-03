8383b2db5010a3c383388ac3c13516b50056b73c
Bryant Wiggins <topwigit@gmail.com>
2023-06-03 14:23:32 -0400

    Changed the width of nav bar to auto

--- src/Header/Navbar.js
+++ src/Header/Navbar.js
@@ -3,8 +3,8 @@ import {FaSearch} from "react-icons/fa";
 
 function Navbar () {
     return (
-    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
-     <Search searchIcon={<FaSearch size="28"/>}/> 
+    <div className="fixed top-0 left-0 h-screen w-auto m-0 flex flex-col bg-gray-900 text-white shadow-lg">
+     <Search searchIcon={<FaSearch size="28" />}/> 
     </div>
     )
 }
--- src/Header/Search.js
+++ src/Header/Search.js
@@ -4,7 +4,7 @@ function Search ({searchIcon}) {
     return (
         <div>
             <form>
-                <input className="flex-auto border-black bg-slate-600" type="search" placeholder="Search..."></input>{searchIcon}
+                <input className="flex-auto border-black bg-slate-600" type="search" placeholder="Search..." ></input>{searchIcon}
             </form>
         </div>
     )
