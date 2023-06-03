0a40eea2cd35586942d2255d8eddabaf06a48ce6
Bryant Wiggins <topwigit@gmail.com>
2023-06-03 14:15:44 -0400

    reorginazed some files in folders and moved icon imports to a closer parent

--- src/Header/Search.js
+++ src/Header/Search.js
@@ -3,9 +3,6 @@
 function Search ({searchIcon}) {
     return (
         <div>
-            <form> {searchIcon}
-                <input className="flex-auto border-black bg-slate-600 top-10"  type="search" placeholder="Search..." ></input>
-            </form>
             <form>
                 <input className="flex-auto border-black bg-slate-600" type="search" placeholder="Search..."></input>{searchIcon}
             </form>
