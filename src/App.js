8fae88c946c08083dac0176a61bbeed1e790dfce
Macolister <bispo.mac@gmail.com>
2023-06-04 19:36:14 -0400

    take all changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from 'react';
-<<<<<<< HEAD
-import Navbar from './Navbar';
 import {FaSearch} from "react-icons/fa";
 import FormOne from './Components/FormOne';
-
-function App() {
-  return (
-    <div>
-      <ReviewList/>
-    </div>
-
-=======
 import Navbar from './Header/Navbar';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Navbar  />
-=======
     <div>
+       <Navbar  />
       <ReviewList/>
     </div>
 
->>>>>>> b3da4e8 (import list to app, call list comp, check css)
->>>>>>> 4831293 (reorginazed some files in folders and moved icon imports to a closer parent)
   );
 }
 
