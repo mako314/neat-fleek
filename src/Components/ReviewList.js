60a1683c162803403523e11a8bffc69ed432d895
Macolister <bispo.mac@gmail.com>
2023-06-04 18:49:11 -0400

    copy plantsy css

---
+++ src/Components/ReviewList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ReviewList(){
+    return (
+        <ul className="cards">{/* render PlantCards components in here */}</ul>
+    )
+}
+
+export default ReviewList;
