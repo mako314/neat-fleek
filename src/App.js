8b07f3a82c6cf537bbb1c3f1315aca2b9fb20576
Macolister <bispo.mac@gmail.com>
2023-06-04 19:16:33 -0400

    add comp ReviewList to app, import components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar';
 import {FaSearch} from "react-icons/fa";
+import ReviewCards from './Components/ReviewCards';
 import FormOne from './Components/FormOne';
 
 function App() {
