1e0ea4aa998132b71c9a5c7da6a5b06a141dde61
Macolister <bispo.mac@gmail.com>
2023-06-04 19:09:55 -0400

    import list to app, call list comp, check css

--- src/App.js
+++ src/App.js
@@ -15,7 +15,14 @@ import Navbar from './Header/Navbar';
 
 function App() {
   return (
+<<<<<<< HEAD
     <Navbar  />
+=======
+    <div>
+      <ReviewList/>
+    </div>
+
+>>>>>>> b3da4e8 (import list to app, call list comp, check css)
 >>>>>>> 4831293 (reorginazed some files in folders and moved icon imports to a closer parent)
   );
 }
