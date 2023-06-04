bfb7c3db220b4835fb685de2da55431627c7e756
Bryant Wiggins <topwigit@gmail.com>
2023-06-04 19:08:11 -0400

    Added home icon

--- src/Header/Navbar.js
+++ src/Header/Navbar.js
@@ -1,13 +1,22 @@
 import ReviewIcon from "./ReviewIcon";
 import Search from "./Search"
-import {FaSearch, FaFileAlt} from "react-icons/fa";
+import {FaSearch, FaFileAlt, FaHome} from "react-icons/fa";
 
 function Navbar () {
     return (
     <div className="fixed top-0 left-0 h-screen w-auto m-0 flex flex-col bg-gray-900 text-white shadow-lg">
      <Search searchIcon={<FaSearch size="28" color="red" />}/> 
+     <HomeIcon homeIcon={<FaHome size="28" />}/>
      <ReviewIcon reviewIcon={<FaFileAlt size="28" />} />
     </div>
+    )
+}
+
+function HomeIcon ({homeIcon}) {
+    return (
+        <div className="sidebar-icon">
+            {homeIcon}
+        </div>
     )
 }
 
