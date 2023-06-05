0c30603d0a2bb3c0c9bcf0ca9b5378ba0f84a1ae
Bryant Wiggins <topwigit@gmail.com>
2023-06-04 20:05:59 -0400

    added nfg logo with css

--- src/Header/Navbar.js
+++ src/Header/Navbar.js
@@ -2,14 +2,15 @@ import ReviewIcon from "./ReviewIcon";
 import Search from "./Search"
 import GuidesIcon from "./GuidesIcon"
 import DiscussionsIcon from "./DiscussionsIcon";
-import nfgLogo from "/home/bryantw/Development/code/neat-fleet/neat-fleet/src/nfgLogo.png"
+import Logo from "./Logo";
+import logo from "/home/bryantw/Development/code/neat-fleet/neat-fleet/src/nfgLogo.png"
 import {FaSearch, FaFileAlt, FaHome, FaDraftingCompass, FaComments } from "react-icons/fa";
 
 //The css for the icons exist in the index.css file. I gave it a class name "sidebar-icon" and applied the styles via @apply in tailwind!
 function Navbar () {
     return (
     <div className="fixed top-0 left-0 h-screen w-auto m-0 flex flex-col bg-gray-900 text-white shadow-lg">
-        <img src={nfgLogo} />
+     <Logo logo={logo} />
      <Search searchIcon={<FaSearch size="28" color="red" />}/> 
      <HomeIcon homeIcon={<FaHome size="28" />}/>
      <ReviewIcon reviewIcon={<FaFileAlt size="28" />} />
@@ -27,6 +28,7 @@ function HomeIcon ({homeIcon}) {
         </div>
     )
 }
+
 
 
 
