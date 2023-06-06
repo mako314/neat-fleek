de8d89f84499d1cdd7d4874b87c7b0beef9e486b
Macolister <bispo.mac@gmail.com>
2023-06-06 15:18:14 -0400

    slight css styling, image rounded, text

--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,15 @@ function Home(){
         // treating this div below like a section, could put everything in <section> this div might do the same thing as sectioning it though
 
     <div className="home-container"> 
-    {/* this could hold the whole home container, but we could section stuff out inside of it */}
+    {/* this could hold the whole home container, but we could section stuff out inside of it 
+    mx-auto w-full max-w-xl md:max-w-7xl object-cover rounded-4
+    */}
 
-        <div className="" id="featured-image">
-                <img src="https://imguscdn.gamespress.com/cdn/files/BlizzardLive/2023/06/012315-b6ae72f6/D4_Lilith_3840x2160_png_jpgcopy.jpg?otf=y&lightbox=y&ex=2023-08-01+03%3A00%3A00&sky=eff1edc08fe4845e5744a0d86f63add336c5b99ea625331da87465b906929854&w=1024&maxheight=4096&mode=pad&format=jpg"
+    <div className="" id="featured-image">
+                <img className=" mx-auto w-full max-w-screen-md md:max-w-7x1 object-contain rounded-xl"
+                src="https://imguscdn.gamespress.com/cdn/files/BlizzardLive/2023/06/012315-b6ae72f6/D4_Lilith_3840x2160_png_jpgcopy.jpg?otf=y&lightbox=y&ex=2023-08-01+03%3A00%3A00&sky=eff1edc08fe4845e5744a0d86f63add336c5b99ea625331da87465b906929854&w=1024&maxheight=4096&mode=pad&format=jpg"
                 alt="Diablo 4"
-                /> image content here
+                />
                 {/* href in here so if you link the picture */}
                 <a href="routingstuffhere"></a> 
                 </div>
@@ -19,21 +22,21 @@ function Home(){
                 {/* they seem to include some extra div after the feat img ID one that wraps this so 3 divs total along with a few section tags and article*/}
 
                 {/* below is used for the gridding */}
-            <div className=""  id="featured-content-container">
-                <div className="" id="featured-text-content">
+            <div className="xl:grid-layout gap-y-0 mt-16 md:mt-24"  id="featured-content-container">
+                <div className="xl:col-span-9" id="featured-text-content">
                     
-                    <h2> Text content here ex is title of review </h2>
+                    <h2 className="text-3xl md:text-5xl text-center  "> A Dive into Diablo 4 </h2>
 
-                    <ul>
-                        <li> Short Description</li>
-                        <li> Date </li>
-                        <li> Location </li>
+                    <ul className="f-body-4 mt-16 md:mt-24">
+                        <li className="indent-16"> The endless battle between the High Heavens and the Burning Hells rages on as chaos threatens to consume Sanctuary. With ceaseless demons to slaughter, countless Abilities to master, nightmarish Dungeons, and Legendary loot, this vast, open world brings the promise of adventure and devastation. Survive and conquer darkness – or succumb to the shadows. </li>
+                        <li> June 6, 2023 </li>
+                        <li> PlayStation 5, Xbox Series X and Series S, PlayStation 4, Xbox One, Microsoft Windows </li>
+
                         {/* all of these can change, setting a template up. */}
                     </ul>
-
                 </div>
             </div>   
-        </div>
+     </div>
 
     )
 
