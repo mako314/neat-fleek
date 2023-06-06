29cdac028a1f6b3871ce0250b2de729c8d9a20f1
Macolister <bispo.mac@gmail.com>
2023-06-05 22:20:15 -0400

    add proper logic to map over tags

--- src/Components/ReviewCards.js
+++ src/Components/ReviewCards.js
@@ -13,7 +13,10 @@ import React from 'react';
 
 function ReviewCards({author, socialMedia, image, gameTitle, tags, review, developer, system, rating}){
 
-    
+    let mappedTags = tags.map((tag) => (
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tag}</span>
+    ))
+
     return (
 
   
@@ -32,9 +35,7 @@ function ReviewCards({author, socialMedia, image, gameTitle, tags, review, devel
   </div>
 
   <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+    {mappedTags}
   </div>
 
 </div>
