8b11e54f3853a9e2271b3889068ed767de694d92
Bryant Wiggins <topwigit@gmail.com>
2023-06-06 11:07:11 -0400

    Deleted FAsearch Icon

--- src/Header/Navbar.js
+++ src/Header/Navbar.js
@@ -5,7 +5,7 @@ import GuidesIcon from "./GuidesIcon"
 import DiscussionsIcon from "./DiscussionsIcon";
 import Logo from "./Logo";
 import logo from "/home/bryantw/Development/code/neat-fleet/neat-fleet/src/logo-transparent-white-halfsize.png"
-import {FaSearch, FaFileAlt, FaHome, FaComments } from "react-icons/fa";
+import {FaFileAlt, FaHome, FaComments } from "react-icons/fa";
 
 //The css for the icons exist in the index.css file. I gave it a class name "sidebar-icon" and applied the styles via @apply in tailwind!
 function Navbar () {
@@ -23,7 +23,7 @@ function Navbar () {
     return (
     <div className="fixed top-0 left-0 h-screen w-auto m-0 flex flex-col bg-gray-900 text-white shadow-lg">
      <Logo logo={logo} />
-     <Search searchIcon={<FaSearch size="28" color="red" />}/> 
+     <Search /> 
      <HomeIcon homeIcon={<FaHome size="28" />}/>
      <ReviewIcon reviewIcon={<FaFileAlt size="28" />} />
      <GuidesIcon />
--- src/Header/Search.js
+++ src/Header/Search.js
@@ -1,6 +1,6 @@
 
 
-function Search ({searchIcon}) {
+function Search ({}) {
     return (
         <div className="">
             <form > 
