a14c0d2dc9a2a8a2c2efb866ce579d45fc2dab43
Macolister <bispo.mac@gmail.com>
2023-06-06 15:57:12 -0400

    change class to className

--- src/Components/ReviewCards.js
+++ src/Components/ReviewCards.js
@@ -14,27 +14,27 @@ import React from 'react';
 function ReviewCards({author, socialMedia, image, gameTitle, tags, review, developer, system, rating}){
 
     let mappedTags = tags.map((tag) => (
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tag}</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tag}</span>
     ))
 
     return (
 
   
 
-<div class="max-w-sm rounded overflow-hidden shadow-lg">
+<div className="max-w-sm rounded overflow-hidden shadow-lg">
 
-  <img class="object-scale-down h-48 w-96" src={image} alt={gameTitle}/>
+  <img className="object-scale-down h-48 w-96" src={image} alt={gameTitle}/>
 
-  <div class="px-6 py-4">
-    <h1 class="font-bold text-xl mb-2">{gameTitle}</h1>
-    <h2 class="font-style: italic font font-semibold text-xs mb-2">{author}</h2>
-    <h3 class ="font-style: italic font font-semibold text-xs mb-2" >{socialMedia}</h3>
-    <p class="text-gray-700 text-base">
+  <div className="px-6 py-4">
+    <h1 className="font-bold text-xl mb-2">{gameTitle}</h1>
+    <h2 className="font-style: italic font font-semibold text-xs mb-2">{author}</h2>
+    <h3 className ="font-style: italic font font-semibold text-xs mb-2" >{socialMedia}</h3>
+    <p className="text-gray-700 text-base">
         Review Content Here
     </p>
   </div>
 
-  <div class="px-6 pt-4 pb-2">
+  <div className="px-6 pt-4 pb-2">
     {mappedTags}
   </div>
 
