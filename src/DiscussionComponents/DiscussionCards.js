11ba042b1b1b2275e725ac107b00011892393793
Macolister <bispo.mac@gmail.com>
2023-06-07 18:54:20 -0400

    changing class to className

--- src/DiscussionComponents/DiscussionCards.js
+++ src/DiscussionComponents/DiscussionCards.js
@@ -12,25 +12,25 @@ function DiscussionCards({author, socialMedia, title, discussion}){
 
 
     return (
-<div class="m-8 grid grid-cols-12">
-  <div class="flex rounded-xl bg-white p-4 col-span-12">
+<div className="m-8 grid grid-cols-12">
+  <div className="flex rounded-xl bg-white p-4 col-span-12">
 
-    <div class="mx-2 flex h-16 w-16 items-center justify-center overflow-hidden rounded-full flex-shrink-0">
+    <div className="mx-2 flex h-16 w-16 items-center justify-center overflow-hidden rounded-full flex-shrink-0">
       <img src="https://us.123rf.com/450wm/sanek13744/sanek137441707/sanek13744170700028/81890650-speech-bubble-flat-vector-icon-discussion-dialog-logo-illustration-business-pictogram-concept.jpg" />
     </div>
 
-    <div class="ml-4 w-full">
+    <div className="ml-4 w-full">
         
-      <div class="flex w-full items-center justify-between">
-        <h2 class="text-lg font-medium">{author}</h2>
+      <div className="flex w-full items-center justify-between">
+        <h2 className="text-lg font-medium">{author}</h2>
         <p>{title}</p>
       </div>
 
-      <div class="mt-4">
+      <div className="mt-4">
         <p>{discussion}</p>
 
-        <div class="mt-4 space-x-2">
-          <button class="px-4 py-2 rounded-xl bg-sky-400 text-white font-medium">{socialMedia}</button>
+        <div className="mt-4 space-x-2">
+          <button className="px-4 py-2 rounded-xl bg-sky-400 text-white font-medium">{socialMedia}</button>
         </div>
 
       </div>
