926efb492131830990988ef1c476792d143c84af
Bryant Wiggins <topwigit@gmail.com>
2023-06-08 19:21:23 -0400

    Gave 100 opacity to text-over-image

--- src/GuideComponents/GuideCards.js
+++ src/GuideComponents/GuideCards.js
@@ -22,17 +22,20 @@ function GuideCards({author, socialMedia, image, gameTitle, tags, guides, develo
 <div className="max-w-sm rounded overflow-hidden shadow-lg bg-antiflashwhite border-green-600 border-2 relative w-[350px]">
 
   <img className="relative w-[500px]" src={image} alt={gameTitle}/>
+  <div className="text-over-image">
+  <h3 className="text-xl text-white font-bold">{gameTitle}</h3>
+  </div>
 
   
 
-  <div className="px-6 py-4">
-    <h1 className="font-bold text-xl mb-2">{gameTitle}</h1>
+  {/* <div className="px-6 py-4">
     
-  </div>
+    
+  </div> */}
 
-  <div className="px-6 pt-4 pb-2">
-    {/* {mappedTags} */}
-  </div>
+  {/* <div className="px-6 pt-4 pb-2">
+    {mappedTags}
+  </div> */}
 
 </div>
     )
